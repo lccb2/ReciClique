0f78b5c6b7a1edb0d8420aeb235594bab27a56a6
luisalll <llll@cin.ufpe.br>
2025-07-18 11:34:52 -0300

    ajustando env no database e env example

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'LuAu1506POST',
-  database: 'reciclique',
-  port: 5433,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   define: {
     timestamps: true,
     underscored: true,
