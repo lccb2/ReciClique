4df6caa3438433246fa2e84a0483bcabfb4f3403
mecbDuda <mecb@cin.ufpe.br>
2025-07-18 00:25:20 -0300

    add nome tabela Materiais.js

--- backend/src/models/Materiais.js
+++ backend/src/models/Materiais.js
@@ -1,12 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const database = require('../config/database');
 
-class User extends Model {
+class Materiais extends Model {
     static init(sequelize) {
         super.init({
             name: DataTypes.STRING,
         }, {
-            sequelize
+            sequelize,
+            tableName: 'materiais',
         })
     }
 
