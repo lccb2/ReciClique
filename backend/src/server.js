13b982ce1567468c7171d6053c61572da3bcc82a
mecbDuda <mecb@cin.ufpe.br>
2025-07-18 00:26:30 -0300

    add comment e pesq (test) server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ const userRoutes = require('./UserService/routes.js');
 const publiRoutes = require('./PostService/routes.js');
 const pagRoutes = require('./PagService/routes.js');
 const autRoutes = require('./AutService/routes.js');
+const comentRoutes = require('./ComentService/routes.js');
+const pesqRoutes = require('./PesqService/routes.js');
 
 require('./database')
 
@@ -16,5 +18,7 @@ app.use(userRoutes);
 app.use(publiRoutes);
 app.use(pagRoutes);
 app.use(autRoutes);
+app.use('/coment', comentRoutes);
+app.use('/pesq', pesqRoutes);
 
 app.listen(3333);
