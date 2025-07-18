8c8629b1b9b1482030cba481a505f1d0226e79ab
mecbDuda <mecb@cin.ufpe.br>
2025-07-18 00:27:17 -0300

    Update routes.js

--- backend/src/ComentService/routes.js
+++ backend/src/ComentService/routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const PostController = require('./ComentController');
+const ComentController = require('./ComentController');
 
 const routes = express.Router();
 
+// comentários
+routes.post('/comment', ComentController.createComment);
+routes.get('/comments/:post_id', ComentController.listComments);
 
+// curtidas
+routes.post('/like', ComentController.like);
+routes.post('/unlike', ComentController.unlike);
+routes.get('/isliked', ComentController.isLiked);
 
 module.exports = routes;
